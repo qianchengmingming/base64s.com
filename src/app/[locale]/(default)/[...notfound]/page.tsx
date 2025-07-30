857801437fcdf4b4705191138a69a75c4b19ab09
qianchengmingming <dajiang_info@163.com>
2025-07-30 21:11:32 +0800

    重新开发项目

--- src/app/[locale]/(default)/[...notfound]/page.tsx
+++
@@ -1,9 +0,0 @@
-import Base64Code from "@/components/home/base64code";
-
-export default function NotFoundCatchAll() {
-    return (
-        <div className="container flex items-center justify-center">
-            <Base64Code />
-        </div>
-    );
-} 
